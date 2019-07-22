45045dbbbc1862670913eb0a76d46e6a1e01b5d0
Talha Kerpicci <44553433+N1ght-Fury@users.noreply.github.com>
2019-07-22 21:31:22 +0300

    Update Post_Database.py

--- Post_Database.py
+++ Post_Database.py
@@ -2,13 +2,12 @@ import sqlite3
 
 class Post():
 
-    def __init__(self,title,writer,date,post_info,link,picture):
+    def __init__(self,img_id,title,writer,post_info,link):
+        self.img_id = img_id
         self.title = title
         self.writer = writer
-        self.date = date
         self.post_info = post_info
         self.link = link
-        self.picture = picture
 
 
     def text_of_mail(self):
@@ -20,17 +19,13 @@ class Post():
         </head>
         <body>
 
-        <a href='""" + self.link + """'><img src='""" + self.picture + """' width="300px" height="155px" alt="Image Poster"><a/>
+        <a href='""" + self.link + """'><img src='""" + 'https://oyungezer.com.tr/' + self.img_id + """' width="300px" height="155px" alt="Image Poster"><a/>
 
         <a href='""" + self.link + """' style="text-decoration: none;"><h1 style="color: orange; max-height: 67px!important; font: 20px/22px 'Contentia Bold', Tahoma, sans-serif; margin-top: 5px; width: 300px;">""" + self.title +"""</h1></a>
 
         <div style="font-size: 12px; margin-top: 5px; width: 300px;">
             <span>
                 """ + self.writer + ' tarafından' + """
-            </span>
-
-            <span>
-                """ + self.date + """
             </span>
         </div>
 
@@ -54,19 +49,30 @@ class Database_Post():
 
         query = "create table if not exists " \
                 "Tbl_Posts (" \
+                "ID text," \
                 "Title text," \
                 "Writer text," \
-                "Date text," \
                 "Info text," \
                 "Link text)"
         self.cursor.execute(query)
         self.connection.commit()
 
-    def check_if_post_exists(self,link):
+    def check_if_post_exists(self, img_id = '', link = ''):
 
         #query = "select * from tbl_posts where link = @p1"
         #self.cursor.execute(query,(link,))
-        query = "select * from tbl_posts where link like '%" + link + "%'"
+
+        query_search = ""
+        value = ""
+
+        if (len(img_id) != 0):
+            query_search = 'ID'
+            value = img_id
+        elif (len(link) != 0):
+            query_search = 'link'
+            value = link
+
+        query = "select * from tbl_posts where " + query_search + " = '" + value + "'"
         self.cursor.execute(query)
         posts = self.cursor.fetchall()
 
@@ -79,8 +85,5 @@ class Database_Post():
     def add_post(self,Post):
 
         query = "insert into tbl_posts values (@p1,@p2,@p3,@p4,@p5)"
-        self.cursor.execute(query,(Post.title,Post.writer,Post.date,Post.post_info,Post.link))
+        self.cursor.execute(query,(Post.img_id,Post.title,Post.writer,Post.post_info,Post.link))
         self.connection.commit()
-
-
-
